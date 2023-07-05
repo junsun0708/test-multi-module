d2cfa9560eef2e15a7bfb1540092803b3ac4ab2d
JYH <salvayh@thingspire.com>
2023-07-05 16:12:39 +0900

    초기 셋팅

---
+++ src/main/java/com/thingspire/testmultimodule/TestMultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.thingspire.testmultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMultiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMultiModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thingspire/testmultimodule/TestMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thingspire.testmultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
